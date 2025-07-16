3f316daf1f4c3a2effb3b31305748612bec9abfe
umang-pandya31 <pandyaumang4372@gmail.com>
2025-07-17 00:23:52 +0530

    Implement search sweet feature

--- SweetShop/views.py
+++ SweetShop/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from model.models import *
 from django.http import HttpResponse
+from django.db.models import Q
+
+def index(request):
+    from django.db.models import Q
 
 def index(request):
     sweets = Sweet.objects.all()
-    return render(request,"index.html",{'sweets': sweets})
+    query = request.GET.get('query')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    if query:
+        sweets = sweets.filter(
+            Q(name__icontains=query) |
+            Q(category__icontains=query)
+        )
+    if min_price:
+        sweets = sweets.filter(price__gte=min_price)  #__gte is predefine function meand greterthan equal to
+    if max_price:
+        sweets = sweets.filter(price__lte=max_price)  #__lte is predefine function meand lessthan equal to
+
+    return render(request, 'index.html', {'sweets': sweets})
+
+    # sweets = Sweet.objects.all()
+    # return render(request,"index.html",{'sweets': sweets})
 
 def add_sweet(request):
     if request.method == "POST":
