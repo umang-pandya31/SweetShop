0ff76343d709176c49d03ae3687a7857e217068c
umang-pandya31 <pandyaumang4372@gmail.com>
2025-07-17 00:15:59 +0530

    Add test for search sweets

--- SweetShop/tests.py
+++ SweetShop/tests.py
@@ -14,7 +14,7 @@ class SweetModelTest(TestCase):
         self.assertEqual(sweet.quantity, 50) #Check value
 
     def test_delete_sweet(self):
-        sweet = models.Sweet.objects.create(
+        sweet = models.Sweet.objects.create(  #Create dummy data
             name="Lollipop",
             category="candy",
             price=2.00,
@@ -25,16 +25,24 @@ class SweetModelTest(TestCase):
         self.assertFalse(models.Sweet.objects.filter(id=sweet_id).exists())
 
     def test_update_sweet(self):
-        sweet = models.Sweet.objects.create(
-            name="Cheesecake",
-            category="pastry",
+        sweet = models.Sweet.objects.create( #Create dummy data
+            name="kitkat",
+            category="chocolate",
             price=15.00,
             quantity=20
         )
         sweet.price = 18.00
         sweet.quantity = 25
         sweet.save()
-
         updated = models.Sweet.objects.get(id=sweet.id)
         self.assertEqual(updated.price, 18.00)
         self.assertEqual(updated.quantity, 25)
+
+    def test_search_sweet(self):
+        models.Sweet.objects.create(name="kitkat", category="chocolate", price=15.00, quantity=20)
+        models.Sweet.objects.create(name="Kit Roll", category="chocolate", price=18.00, quantity=15)
+
+        sweets = models.Sweet.objects.filter(category="chocolate")
+
+        for sweet in sweets:  #we iterat loop because match sweets is more than one
+            self.assertEqual(sweet.category, "chocolate")
